702ddaf5716dda33c9b2dd3035000e32a885c567
Victor Monzón <monzonanglasv@gmail.com>
2018-04-30 01:12:42 -0500

    Facebook

--- Eventsapp/app/src/main/java/com/example/viict/eventsapp/MainActivity.java
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/MainActivity.java
@@ -6,34 +6,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/*import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;*/
+
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG ="";
-    private FirebaseAuth mAuth;
-    private EditText emailFiel, passwordFiel;
-    private Button loginBtn;
 
+    //INTERFAZ
+    private EditText emailField, passwordField;
+    private Button btnLogin;
+    //private LoginButton lbFacebook;
+
+   //FIREBASE
+    private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //FACEBOOK
+    //private CallbackManager mCallbackManager;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        emailFiel = (EditText) findViewById(R.id.email);
-        passwordFiel = (EditText) findViewById(R.id.password);
+        //CONEXION ENTRE INTERFAZ Y VARIABLES
+        emailField = (EditText) findViewById(R.id.email);
+        passwordField = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.login);
+        //lbFacebook = (LoginButton) findViewById(R.id.login_button);
 
+        //INSTANCIA LA CONEXION CON FIREBASE
         mAuth = FirebaseAuth.getInstance();
 
-        loginBtn = (Button) findViewById(R.id.login);
+        //INSTANCIA LA CONEXION A FACEBOOK
+        //mCallbackManager = CallbackManager.Factory.create();
 
+
+        //ACCESO A LA SEGUNDA PANTALLA
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -42,23 +75,77 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+        //ACCIÓN DE CLICK AL BOTON INGRESAR
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LogearUsuario();
             }
         });
 
+        /*//FACEBOOK
+        lbFacebook.setReadPermissions("email", "public_profile");
+
+        lbFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });*/
     }
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }*/
+
+
+    /*private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }*/
+
+
 
     private void LogearUsuario(){
 
-        String email = emailFiel.getText().toString();
-        String password = passwordFiel.getText().toString();
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -74,4 +161,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
 }
