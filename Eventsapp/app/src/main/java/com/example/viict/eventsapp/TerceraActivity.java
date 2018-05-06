cc19c800be50a6477da2283ad7dab54f5a4072ce
Victor Monzón <monzonanglasv@gmail.com>
2018-05-06 08:50:59 -0500

    Interfaz formulario, cree variaables, aun no funciona el boton registar, pero si el Volver

--- Eventsapp/app/src/main/java/com/example/viict/eventsapp/TerceraActivity.java
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/TerceraActivity.java
@@ -5,18 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class TerceraActivity extends AppCompatActivity {
-
+    private EditText nombres;
+    private EditText apellidos;
+    private EditText fechaNac;
+    private  EditText email;
+    private EditText password;
+    private Button registar;
     private Button volver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercera);
-
+        nombres= (EditText) findViewById(R.id.nombres);
+        apellidos= (EditText) findViewById(R.id.apellidos);
+        fechaNac= (EditText) findViewById(R.id.fechaNac);
+        email= (EditText) findViewById(R.id.email);
+        password= (EditText) findViewById(R.id.password);
+        registar= (Button) findViewById(R.id.registrar);
         volver = (Button) findViewById(R.id.volver);
-
+        
+        registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TerceraActivity.this, SegundaActivity.class));
+            }
+        });
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
