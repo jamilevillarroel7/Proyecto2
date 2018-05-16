b81c09f33a2cca5f510976399e47080eaff0d7ed
Victor Monzón <monzonanglasv@gmail.com>
2018-05-15 23:17:09 -0500

    Layouts

---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/Cliente/CL05ListaEventosActivity.java
@@ -0,0 +1,15 @@
+package com.example.viict.eventsapp.Cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.viict.eventsapp.R;
+
+public class CL05ListaEventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cl05_lista_eventos);
+    }
+}
---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/Cliente/CL06ListadeEventosDetalleActivity.java
@@ -0,0 +1,15 @@
+package com.example.viict.eventsapp.Cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.viict.eventsapp.R;
+
+public class CL06ListadeEventosDetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cl06_listade_eventos_detalle);
+    }
+}
