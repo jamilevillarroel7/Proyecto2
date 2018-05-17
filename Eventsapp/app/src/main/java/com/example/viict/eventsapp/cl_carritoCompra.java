0864fe0e79265c5e350f173ac0a6e3e9fa611796
Victor Monzón <monzonanglasv@gmail.com>
2018-05-16 23:34:51 -0500

    Cattiyo de compra y Lista de eventos

---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/cl_carritoCompra.java
@@ -0,0 +1,13 @@
+package com.example.viict.eventsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cl_carritoCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cl_carrito_compra);
+    }
+}
---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/org_detalleOrganizacion.java
@@ -0,0 +1,13 @@
+package com.example.viict.eventsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class org_detalleOrganizacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_detalle_organizacion);
+    }
+}
