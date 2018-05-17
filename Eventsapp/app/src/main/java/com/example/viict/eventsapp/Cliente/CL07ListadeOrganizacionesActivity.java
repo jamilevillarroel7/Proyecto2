5ae1b8424ad45d12b9b66ef499fc44946d0ec69e
Victor Monzón <monzonanglasv@gmail.com>
2018-05-16 22:07:41 -0500

    mas Layouts

---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/Cliente/CL07ListadeOrganizacionesActivity.java
@@ -0,0 +1,15 @@
+package com.example.viict.eventsapp.Cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.viict.eventsapp.R;
+
+public class CL07ListadeOrganizacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cl07_listade_organizaciones);
+    }
+}
---
+++ Eventsapp/app/src/main/java/com/example/viict/eventsapp/Cliente/CL09RecordatoriosActivity.java
@@ -0,0 +1,15 @@
+package com.example.viict.eventsapp.Cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.viict.eventsapp.R;
+
+public class CL09RecordatoriosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cl09_recordatorios);
+    }
+}
